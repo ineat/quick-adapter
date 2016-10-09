cff482ddf38ca097bf4dede0dd2b217e40c166bf
Mehdi Slimani <mehdi.slimani.developer@gmail.com>
2016-10-09 14:22:06 +0200

    feat(QuickAdapter): init library

---
+++ quickadapter/src/main/java/com/ineat/quickadapter/QuickLayout.java
@@ -0,0 +1,12 @@
+package com.ineat.quickadapter;
+
+import android.support.annotation.LayoutRes;
+
+/**
+ * Created by mslimani on 08/10/2016.
+ */
+public @interface QuickLayout {
+
+    @LayoutRes int value();
+
+}
